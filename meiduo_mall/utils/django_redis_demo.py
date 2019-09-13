ebb1094f5b02ab82f5c704af916cdd5e4ec0f431
xwp <xwp_fullstack@163.com>
2019-09-13 10:50:43 +0800

    7.django 基本使用

---
+++ meiduo_mall/utils/django_redis_demo.py
@@ -0,0 +1,13 @@
+
+
+def test_django_redis():
+    #1.导包
+    from django_redis import get_redis_connection
+
+    #2.链接
+    client = get_redis_connection('default')
+
+    #3.增删改查
+    client.set('django_redis_key','itcast')
+
+    print(client.get('django_redis_key'))
