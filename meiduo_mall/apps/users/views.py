b90c7319cb84c0c203668b5686cb3c291f330d97
黄铭 <13792672387@163.com>
2019-09-14 08:19:01 +0800

    zi

---
+++ meiduo_mall/apps/users/views.py
@@ -0,0 +1,65 @@
+import re
+
+from django import http
+
+from django.shortcuts import render, redirect
+from django.views import View
+
+
+
+
+class RegisterView(View):
+
+    def get(self,request):
+
+        return render(request,'register.html')
+    def post(self,request):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        password2 = request.POST.get('password2')
+        mobile = request.POST.get('mobile')
+        allow = request.POST.get('allow')
+
+        # 判断参数是否齐全
+        if not all([username, password, password2, mobile, allow]):
+            return http.HttpResponseForbidden('缺少必传参数')
+        # 判断用户名是否是5-20个字符
+        if not re.match(r'^[a-zA-Z0-9_-]{5,20}$', username):
+            return http.HttpResponseForbidden('请输入5-20个字符的用户名')
+        # 判断密码是否是8-20个数字
+        if not re.match(r'^[0-9A-Za-z]{8,20}$', password):
+            return http.HttpResponseForbidden('请输入8-20位的密码')
+        # 判断两次密码是否一致
+        if password != password2:
+            return http.HttpResponseForbidden('两次输入的密码不一致')
+        # 判断手机号是否合法
+        if not re.match(r'^1[3-9]\d{9}$', mobile):
+            return http.HttpResponseForbidden('请输入正确的手机号码')
+        # 判断是否勾选用户协议
+        if allow != 'on':
+            return http.HttpResponseForbidden('请勾选用户协议')
+
+        from apps.users.models import User
+        user = User.objects.create_user(username=username,password=password,mobile=mobile)
+
+        from django.contrib.auth import login
+        login(request, user)
+
+
+
+
+        return redirect('/')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
